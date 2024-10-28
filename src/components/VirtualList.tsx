7a60543aa431508c4a7e5c1f2055a70d7303c7b8
xREMAGIx <tramdo2806@gmail.com>
2024-10-29 00:29:02 +0700

    :hammer: enhance artefact list

--- src/components/VirtualList.tsx
+++ src/components/VirtualList.tsx
@@ -1,22 +1,53 @@
-import { createMemo } from "solid-js";
+import { createMemo, type JSXElement } from "solid-js";
 import { VList } from "virtua/solid";
+import { useStore } from "@nanostores/solid";
+import {
+  rarityFilter,
+  typeFilter,
+  tagFilter,
+  nameFilter,
+} from "@stores/artefactFilterStore";
 
-interface VirtualListProps {}
+export const ArtefactVirtualList = ({ artefacts }: any) => {
+  if (!artefacts) {
+    return null;
+  }
 
-export const ArtefactVirtualList = ({
-  artefacts,
-  filters,
-}: VirtualListProps) => {
   const frozenArtefacts = [...artefacts.children];
-  const data = Array.from({ length: frozenArtefacts.length });
+
+  const $rarityFilter = useStore(rarityFilter);
+  const $typeFilter = useStore(typeFilter);
+  const $tagFilter = useStore(tagFilter);
+  const $nameFilter = useStore(nameFilter);
+
   const list = createMemo(() => {
-    return frozenArtefacts;
+    const rarityFilter = $rarityFilter();
+    const typeFilter = $typeFilter();
+    const tagFilter = $tagFilter();
+    const nameFilter = $nameFilter();
+
+    const filteredArtefacts = frozenArtefacts.filter((ele) => {
+      const rarity = ele.getAttribute("data-artefact-rarity");
+      const type = ele.getAttribute("data-artefact-type");
+      const tags = ele.getAttribute("data-artefact-tags");
+      const name = ele.getAttribute("data-artefact-name");
+
+      const filters =
+        (rarityFilter === "ALL" || rarity === rarityFilter) &&
+        (typeFilter === "ALL" || type === typeFilter) &&
+        (tagFilter === "ALL" || tags.includes(tagFilter));
+
+      return !nameFilter
+        ? filters
+        : filters && name.toLowerCase().includes(nameFilter.toLowerCase());
+    });
+
+    return filteredArtefacts;
   });
 
   return (
     <>
-      {filters}
-      <VList data={data} style={{ height: "50vh" }}>
+      <VList data={list()} style={{ height: "50vh" }}>
         {(_, i) => <div data-index={i}>{list()[i]}</div>}
       </VList>
     </>
---
+++ src/stores/artefactFilterStore.ts
@@ -0,0 +1,6 @@
+import { atom } from "nanostores";
+
+export const rarityFilter = atom("ALL");
+export const typeFilter = atom("ALL");
+export const tagFilter = atom("ALL");
+export const nameFilter = atom("");
