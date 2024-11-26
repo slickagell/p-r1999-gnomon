eb2f4b89cbc9e12208471be875f515fad5af2856
xREMAGIx <tramdo2806@gmail.com>
2024-11-26 11:27:27 +0700

    :sparkles: implement pagefind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import alpinejs from "@astrojs/alpinejs";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
 import { defineConfig } from "astro/config";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
@@ -11,6 +12,9 @@ import { directivePlugin } from "./src/scripts/directive-plugin.mjs";
 
 // https://astro.build/config
 export default defineConfig({
+  build: {
+    format: "file",
+  },
   integrations: [
     tailwind(),
     alpinejs(),
@@ -21,6 +25,7 @@ export default defineConfig({
         [rehypeAutolinkHeadings, { behavior: "wrap", test: ["h2", "h3"] }],
       ],
     }),
+    pagefind(),
     solidJs(),
   ],
   devToolbar: {
