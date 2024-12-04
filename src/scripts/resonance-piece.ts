49e2d86a2922694c66e9dd89221189a986382757
xREMAGIx <tramdo2806@gmail.com>
2024-12-04 17:03:03 +0700

    :hammer: enhance resonance board & update characters content

--- src/scripts/resonance-piece.ts
+++ src/scripts/resonance-piece.ts
@@ -1,5 +1,6 @@
 import { STATS } from "@constants/character";
-import { IMAGE_ORIENTATION, RESONANCE_PIECES } from "@constants/resonance";
+import { IMAGE_ORIENTATION } from "@constants/resonance";
+import RESONANCE_PIECES from "@data/resonance/common/pieces.json";
 import Alpine from "alpinejs";
 import matrix from "matrix-js";
 import { changeImageOrientation } from "./common";
@@ -52,7 +53,7 @@ export default ({
           },
         };
       },
-      {},
+      {}
     );
   },
 
@@ -65,7 +66,7 @@ export default ({
     this.quantity = newQuantity;
 
     const generalStatsEl = document.querySelector(
-      "#generalStats",
+      "#generalStats"
     ) as HTMLDivElement;
     if (!generalStatsEl) return;
     let generalStatsAlpineData: any = Alpine.$data(generalStatsEl);
@@ -83,7 +84,7 @@ export default ({
     const container = this.$el;
     const wrapper = container.querySelector("div[data-piece-id=" + id + "]");
     const image = container.querySelector(
-      "img[data-piece-img-id=" + id + "]",
+      "img[data-piece-img-id=" + id + "]"
     ) as HTMLImageElement;
 
     if (!image || !wrapper) return;
