2789246efe4664c327228a2be3c7309b29865b5e
xREMAGIx <tramdo2806@gmail.com>
2025-02-06 01:41:20 +0700

    :pencil: update character resonance materials content

--- src/scripts/resonance-piece.ts
+++ src/scripts/resonance-piece.ts
@@ -56,7 +56,7 @@ export default ({
           },
         };
       },
-      {}
+      {},
     );
   },
 
@@ -69,7 +69,7 @@ export default ({
     this.quantity = newQuantity;
 
     const generalStatsEl = document.querySelector(
-      "#generalStats"
+      "#generalStats",
     ) as HTMLDivElement;
     if (!generalStatsEl) return;
     let generalStatsAlpineData: any = Alpine.$data(generalStatsEl);
@@ -87,7 +87,7 @@ export default ({
     const container = this.$el;
     const wrapper = container.querySelector("div[data-piece-id=" + id + "]");
     const image = container.querySelector(
-      "img[data-piece-img-id=" + id + "]"
+      "img[data-piece-img-id=" + id + "]",
     ) as HTMLImageElement;
 
     if (!image || !wrapper) return;
@@ -139,7 +139,7 @@ export default ({
   showPieceStat(statKey: string, stats) {
     if (this.isMainPiece) {
       const attributesEl = document.querySelector(
-        "#attributes"
+        "#attributes",
       ) as HTMLDivElement;
       if (!attributesEl) return;
 
