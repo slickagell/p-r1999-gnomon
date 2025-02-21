a8220c62b7d5dc2144b689e52ad5d9521ee157cf
xREMAGIx <tramdo2806@gmail.com>
2025-02-21 14:45:32 +0700

    :pencil: update reveries in the rain content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require("tailwindcss/plugin");
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
       colors: {
+        black: "#1b1b1b",
+
         mineral: "#8D6838",
         star: "#4D719A",
         plant: "#418D51",
@@ -48,6 +52,11 @@ export default {
       gridTemplateColumns: {
         filter: "auto 1fr",
       },
+      textShadow: {
+        sm: "0 1px 2px var(--tw-shadow-color)",
+        DEFAULT: "0 2px 4px var(--tw-shadow-color)",
+        lg: "0 8px 16px var(--tw-shadow-color)",
+      },
     },
   },
   daisyui: {
@@ -58,7 +67,7 @@ export default {
           secondary: "#e9dccd",
           accent: "#1fb2a6",
           neutral: "#2a323c",
-          "base-100": "#000000",
+          "base-100": "#1b1b1b",
           info: "#3abff8",
           success: "#36d399",
           warning: "#fbbd23",
@@ -73,5 +82,17 @@ export default {
       },
     ],
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      );
+    }),
+  ],
 };
