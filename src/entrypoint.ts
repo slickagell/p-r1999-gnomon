5b62fc7ed7972d9e6b384b24f7d949d6d990976f
xREMAGIx <tramdo2806@gmail.com>
2025-02-28 09:53:43 +0700

    :hammer: update layout

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,23 +1,35 @@
-//x-node based on https://jacksleight.dev/posts/alpine-x-node-directive
-
 import type { Alpine } from "alpinejs";
 
 export default (Alpine: Alpine) => {
-  Alpine.directive("node", (el, { modifiers, expression }) => {
-    let evaluate = Alpine.evaluateLater(el, expression);
-    Alpine.effect(() => {
-      evaluate((value) => {
-        Alpine.mutateDom(() => {
-          if (value) {
-            const node = modifiers.includes("clone")
-              ? (value as any).cloneNode(true)
-              : value;
-            el.replaceChildren(node);
-          } else {
-            el.replaceChildren();
-          }
+  Alpine.directive(
+    "node",
+    (el, { modifiers, expression }, { evaluateLater, effect }) => {
+      let evaluate = evaluateLater(expression);
+      effect(() => {
+        evaluate((value) => {
+          Alpine.mutateDom(() => {
+            if (value) {
+              if (modifiers.includes("clone-children")) {
+                const node = (value as any).cloneNode(true);
+
+                let childNodes = Array.from(node.childNodes);
+                childNodes.forEach((child: Node) => {
+                  el.appendChild(child);
+                });
+                return;
+              }
+
+              const node = modifiers.includes("clone")
+                ? (value as any).cloneNode(true)
+                : value;
+
+              el.replaceChildren(node);
+            } else {
+              el.replaceChildren();
+            }
+          });
         });
       });
-    });
-  });
+    }
+  );
 };
