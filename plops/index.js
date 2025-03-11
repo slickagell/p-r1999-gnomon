d08263075a34c7e5a05344dd30df0456154cb257
xREMAGIx <tramdo2806@gmail.com>
2025-03-11 10:12:08 +0700

    :pencil: update willow psychube and team recommendations section

--- plops/index.js
+++ plops/index.js
@@ -3,5 +3,6 @@ import artefact from "./artefact.js";
 import effect from "./effect.js";
 import material from "./material.js";
 import mindmap from "./myth-manifest-mindmap.js";
+import psychube from "./psychube.js";
 
-export { character, artefact, effect, material, mindmap };
+export { character, artefact, effect, material, mindmap, psychube };
